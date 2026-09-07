6963c6f079dbf3b7c132ec0e5612891cf240dedf
tygo60 <tygovonder66@gmail.com>
2026-09-07 12:18:30 +0200

    Einde les
    07/09/2026

--- Week 1 - Hello World/sketch.js
+++ Week 1 - Hello World/sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+
+  // Horizontale lijnen
+background(240);
+strokeWeight(3);
+stroke(0);
+line(0, 0, 600, 0);
+line(0, 100, 600, 100);
+line(0, 150, 600, 150);
+line(0, 200, 600, 200);
+line(0, 250, 600, 250);
+line(0, 300, 600, 300);
+line(0, 350, 600, 350);
+line(0, 400, 600, 400);
+
+// Verticale lijnen
+line(0, 0, 0, 400);
+line(100, 0, 100, 400);
+line(150, 0, 150, 400);
+line(200, 0, 200, 400);
+line(250, 0, 250, 400);
+line(300, 0, 300, 400);
+line(350, 0, 350, 400);
+line(400, 0, 400, 400);
+line(450, 0, 450, 400);
+line(500, 0, 500, 400);
+line(550, 0, 550, 400);
+line(600, 0, 600, 400);
+line(650, 0, 650, 400);
+
+
 }
